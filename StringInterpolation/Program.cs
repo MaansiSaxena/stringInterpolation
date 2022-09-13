4587dd841964423c7e34e629ca99e8788b66dfb1
Mansi Saxena <Mansi.Saxena@bhavnacorp.com>
2022-09-13 16:17:46 +0530

    first commit

---
+++ StringInterpolation/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace StringInterpolation
+{ 
+    class employee
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            string name = "Mansi";
+            var date = DateTime.Now;
+
+            //normal printing methods:
+            Console.WriteLine("Hello " + name + " it is " + DateTime.Now);
+            Console.WriteLine("Hello {0} Today is {1} ", name, DateTime.Now);
+
+            //string interpolation    suitable for long paragraph
+            Console.WriteLine($"Hello {name} Today date is {DateTime.Now}");
+            */
+
+            employee emp = null;
+            //string employee = (emp != null) ? emp.name : null; //
+            emp?.name
+            Console.WriteLine(employee);
+        }
+    }
+}
